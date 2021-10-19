516d055a3ec233b4edc556c2a8d22d44583f83cf
Takiuddin Ahmed <takiuddinahmed@gmail.com>
2021-10-19 17:52:32 +0600

    day7 final

--- Day7/inheritance.dart
+++ Day7/inheritance.dart
@@ -1,27 +1,47 @@
 class Man{
-  String? name;
+  String name = "default";
   int? age;
-  Man(String n, int a){
-    name = n;
-    age = a;
+  Man(String name, int _age){
+    this.name = name;
+    this.age = _age;
   }
   void sayName(){
     print("Hey! I'm $name");
   }
+  void introduce(){
+    print("Hey! I'm $name, $age years old");
+  }
+
+  // setter 
+  void setName(String _name){
+    name = _name;
+  }
+  // getter
+  String getName(){
+    return name;
+  }
+
 }
 class Student extends Man {
   int? roll;
-
-  Student(String n, int a, int r):super(n,a){
-    roll = r;
+  Student(String _name, int _age, int _roll):super(_name,_age){
+    roll = _roll;
   }
-
   void sayRoll(){
     print("Hey! my roll is $roll");
+  }
+  @override
+  void introduce(){
+    print("Hey! I'm $name, $age years old and my roll is $roll");
   }
 }
 void main() {
   var std = new Student("Chatro",234,890);
-  std.sayName();
-  std.sayRoll();
+  // std.name = "Mr. Chatro";
+  std.setName("Mr. Chatro");
+  print(std.getName());
+  std.introduce();
 }
+
+
+// getter setter <- get, set
---
+++ Day7/scope.dart
@@ -0,0 +1,21 @@
+
+void main(){
+  int i = 0;
+  while(false){
+    print(i);
+    i++;
+  }
+}
+
+bool isEven(int n){
+  bool result = false;
+  if(n % 2 == 0){
+    result = true;
+  }
+  return result;
+}
+
+// boolean -- true, false
+// a > b
+// odd even
+
